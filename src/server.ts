7b5feacce7194f2594779722ae738d696ed99dc0
Antoine <antoine@w7th.com>
2025-09-02 16:48:30 -0400

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
-import { Server } from "@modelcontextprotocol/sdk/server";
+import { Server } from "@modelcontextprotocol/sdk/server/index";
 import { ListToolsRequestSchema, CallToolRequestSchema } from "@modelcontextprotocol/sdk/types.js";
 
 import {
